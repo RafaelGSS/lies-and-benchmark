12b46e4df59b69a1ff3e7c6f54a3bd67eba50802
RafaelGSS <rafael.nunu@hotmail.com>
2022-10-12 18:58:10 -0300

    feat: add jmeter test

--- coordinate-omission.js
+++ coordinate-omission.js
@@ -1,10 +1,19 @@
 const http = require('http');
 
+let count = 0;
 const requestListener = function (req, res) {
-  setTimeout(() => {
-    res.writeHead(200);
-    res.end('Hello, World!');
-  }, 500)
+  // 60sec
+  if (++count > 120) {
+    setTimeout(() => {
+      res.writeHead(200);
+      res.end('Hello, World!');
+    }, 59000)
+  } else {
+    setTimeout(() => {
+      res.writeHead(200);
+      res.end('Hello, World!');
+    }, 500)
+  }
 }
 
 const server = http.createServer(requestListener);
