76a261381ab1fae378bc24ff1853d39e48e8bcde
RafaelGSS <rafael.nunu@hotmail.com>
2022-11-21 11:23:01 -0300

    update: closeSync after threshould

--- file-descriptor-limit/limit.js
+++ file-descriptor-limit/limit.js
@@ -1,12 +1,16 @@
 const fs = require('fs')
 
+// MacOS you can use the Console to see failure logs
 console.time('perf')
 const fds = []
-for (let i = 0; i < 61450; ++i) {
+// for (let i = 0; i < 61450; ++i) {
+for (let i = 0; i < 60000; ++i) {
   fs.open('./test' + i, 'w+', (err, fd) => {
     if (!err) fds.push(fd)
   })
 }
-
-fds.map((fd) => fs.closeSync(fd))
 console.timeEnd('perf')
+
+setTimeout(() => {
+  fds.map((fd) => fs.closeSync(fd))
+}, 3000)
